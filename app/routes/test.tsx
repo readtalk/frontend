7c870c794766b8a44e05d4d8b1da996f96b41c87
READTalk <redtalk.app@hotmail.com>
2026-06-12 23:58:50 +0700

    Add Test component to display working message

---
+++ app/routes/test.tsx
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <div>
+      <h1>READTalk Frontend is Working!</h1>
+      <p>If you see this, the worker is running correctly.</p>
+    </div>
+  );
+}
