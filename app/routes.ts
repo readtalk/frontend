66f6c9c27772e67b5ddd5df39751f60b2e2de7ef
READTalk <redtalk.app@hotmail.com>
2026-07-07 14:38:55 +0700

    Reorder routes for login and register

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,8 @@
 import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
-export default [    
-    route("register", "routes/auth/register.tsx"),
-    route("login", "routes/auth/login.tsx"),    
+export default [
+    route("login", "routes/auth/login.tsx"),
+    route("register", "routes/auth/register.tsx"),        
 
     layout("components/layouts/AuthLayout.tsx", [
         route("overview", "routes/overview.tsx"),
