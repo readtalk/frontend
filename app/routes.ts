d4b2238c99cd9a7a2b3feaeaf38eae3412df24f4
READTalk <redtalk.app@hotmail.com>
2026-07-07 16:22:58 +0700

    Fix formatting and ensure consistent route definition

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 export default [
     route("login", "routes/auth/login.tsx"),
-    route("register", "routes/auth/register.tsx"),        
+    route("register", "routes/auth/register.tsx"),
 
     layout("components/layouts/AuthLayout.tsx", [
         route("overview", "routes/overview.tsx"),
