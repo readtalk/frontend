9a1f2f482827d8f51e9374a0b17215387e86dc21
READTalk <redtalk.app@hotmail.com>
2026-07-09 00:48:30 +0700

    Add route configurations for authentication and overview

---
+++ app/routes.ts
@@ -0,0 +1,13 @@
+import { type RouteConfig, layout, route } from "@react-router/dev/routes";
+
+export default [    
+    route("", "routes/welcome.tsx"),  
+    route("login", "routes/auth/login.tsx"),
+    route("register", "routes/auth/register.tsx"),
+
+    layout("components/layouts/AuthLayout.tsx", [
+        route("overview", "routes/overview.tsx"),
+        route("channel/:id", "routes/channel.tsx"),
+        route("profile", "routes/profile.tsx"), // 🔥 TAMBAHKAN INI
+    ]),
+] satisfies RouteConfig;
