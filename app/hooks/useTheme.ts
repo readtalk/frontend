3388e421dee74308a20bb937a8e49a15ea239c5a
READTalk <redtalk.app@hotmail.com>
2026-07-08 12:46:07 +0700

    Create useTheme.ts

---
+++ app/hooks/useTheme.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react'
+
+const useTheme = (theme?: 'dark' | 'light') => {
+  useEffect(() => {
+    const html = document.querySelector('html')
+    const metaThemeColor = document.querySelector('meta[name="theme-color"]')
+
+    
+    if (theme === 'dark') {
+      html?.classList.add('dark')
+    } else if (theme === 'light' && html?.classList.contains('dark')) {
+      html.classList.remove('dark')
+    }
+
+    
+    if (metaThemeColor) {
+      metaThemeColor.setAttribute('content', theme === 'dark' ? '#111b21' : '#ffffff')
+    }
+  }, [theme])
+}
+
+export default useTheme
