669882ab019470c03d462da0498ec3aed2aef545
READTalk <redtalk.app@hotmail.com>
2026-07-16 19:48:11 +0700

    Update import from 'react-router' to 'react-router-dom'

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 import { Avatar } from '~/components/avatar/Avatar';
